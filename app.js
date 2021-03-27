bc68e3786b5949033346638ee8304f949fb2a63c
admin <admin@SELF>
2021-03-27 11:49:54 +0300

    Add project files.

---
+++ app.js
@@ -0,0 +1,19 @@
+﻿const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+    const randomNum = getRandomNum();
+    console.log(randomNum);
+
+    document.body.style.background = colors[randomNum];
+    color.textContent = colors[randomNum];
+});
+/* Random number generator */
+function getRandomNum() {
+    return Math.floor(Math.random() * colors.length);
+
+}
+
